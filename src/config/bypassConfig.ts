2ecee19d642127eecb94cdf25917fe0cc5afd284
brunorusso2607-dev <brunorusso2607@gmail.com>
2026-01-25 22:58:54 -0300

    feat: habilita UI do Stripe para mostrar plano na home

--- src/config/bypassConfig.ts
+++ src/config/bypassConfig.ts
@@ -6,7 +6,7 @@
  * When true, hides all Stripe/payment related UI elements
  * Set to false to re-enable payment UI
  */
-export const HIDE_STRIPE_UI = true;
+export const HIDE_STRIPE_UI = false;
 
 /**
  * When true, all logged-in users get premium access without payment
